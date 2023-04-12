a1c209b2f23c14ff65f580e32aa683e180c3ede8
심재원 <csappletail@gmail.com>
2023-04-12 16:23:26 +0900

    [FE] init: react 기본 세팅 완료
    이슈 : 엾음

---
+++ front/src/apis/axiosInstance.tsx
@@ -0,0 +1,32 @@
+import axios, { AxiosInstance } from 'axios'
+
+export const interceptors = (instance: AxiosInstance) => {
+  instance.interceptors.request.use(
+    (config) => {
+      // access token 헤더 삽입 코드
+      const token = localStorage.getItem('accessToken')
+      config.headers.Authorization = `Bearer ${token}`
+      return config
+    },
+    (error) => Promise.reject(error.response)
+  )
+  return instance
+}
+
+const BASE_URL = '' // 서버 기입 필요
+
+// 단순 get요청으로 인증값이 필요없는 경우
+const axiosApi = (url: string, options?: object) => {
+  const instance = axios.create({ baseURL: url, ...options })
+  return instance
+}
+
+// post, delete등 api요청 시 인증값이 필요한 경우
+const axiosAuthApi = (url: string, options?: object) => {
+  const instance = axios.create({ baseURL: url, ...options })
+  interceptors(instance)
+  return instance
+}
+
+export const axBase = axiosApi(BASE_URL)
+export const axAuth = axiosAuthApi(BASE_URL)
