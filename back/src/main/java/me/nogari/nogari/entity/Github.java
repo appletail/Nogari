015566d4acf833b0817836b20510c03d2145ecfe
박시형 <qkrtlgud3286@naver.com>
2023-04-13 14:12:00 +0900

    [BE] feat: 초기 entity  작성
    - entity 작성
    
    이슈: S08P31C206-68

---
+++ back/src/main/java/me/nogari/nogari/entity/Github.java
@@ -0,0 +1,44 @@
+package me.nogari.nogari.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Github extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "github_id")
+	private Long githubId;
+
+	@Column(name = "filename", length = 50)
+	private String filename;
+
+	@Column(name = "repository", length = 50)
+	private String repository;
+
+	@Column(name = "status", length = 15, nullable = false)
+	private String status;
+
+	@Column(name = "link", length = 300)
+	private String link;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+}
