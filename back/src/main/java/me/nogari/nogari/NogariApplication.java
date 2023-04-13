49730a1c20757247cc645e843100851c95cf04ee
이광용 <dnflrhkddyd@naver.com>
2023-04-13 11:12:36 +0900

    [BE] feat: BE 프로젝트 생성
    이슈: S08P31C206-1

---
+++ back/src/main/java/me/nogari/nogari/NogariApplication.java
@@ -0,0 +1,13 @@
+package me.nogari.nogari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NogariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NogariApplication.class, args);
+	}
+
+}
