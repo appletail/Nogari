815363a9a4c51272d4e23a02c091492989482afa
박시형 <qkrtlgud3286@naver.com>
2023-04-13 14:58:41 +0900

    [BE] feat: swagger ui 적용
    - swagger ui 적용
    - db password 변경
    
    이슈: S08P31C206-68

---
+++ back/src/main/java/me/nogari/nogari/api/controller/TestController.java
@@ -0,0 +1,21 @@
+package me.nogari.nogari.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/test")
+public class TestController {
+
+	@GetMapping
+	public String Test(@RequestParam String test) {
+		return test;
+	}
+}
