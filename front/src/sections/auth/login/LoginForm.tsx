5c5bff5e70d7a51fb565f16df8ed2f5c19e7571b
정하림 <harimchung1998@gmail.com>
2023-04-14 17:22:46 +0900

    [FE] feat: login page 추가
    이슈: S08P31C206-

---
+++ front/src/sections/auth/login/LoginForm.tsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+// @mui
+import { LoadingButton } from '@mui/lab'
+import {
+  Link,
+  Stack,
+  IconButton,
+  InputAdornment,
+  TextField,
+} from '@mui/material'
+
+import Iconify from '@/components/iconify'
+
+function LoginForm() {
+  const navigate = useNavigate()
+
+  const [showPassword, setShowPassword] = useState(false)
+
+  // replace : true 를 적용해서 뒤로가기가 안되게 적용하였습니다.
+  const handleClick = () => {
+    navigate('/test', { replace: true })
+  }
+  return (
+    <>
+      <Stack spacing={3}>
+        <TextField label="Email address" name="email" />
+
+        <TextField
+          label="Password"
+          name="password"
+          type={showPassword ? 'text' : 'password'}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  edge="end"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  <Iconify
+                    icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'}
+                  />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <LoadingButton
+          fullWidth
+          size="large"
+          type="submit"
+          variant="contained"
+          onClick={handleClick}
+        >
+          Login
+        </LoadingButton>
+      </Stack>
+
+      {/* 여기에 추후 로딩이 들어갑ㄴ디ㅏ. */}
+    </>
+  )
+}
+
+export default LoginForm
