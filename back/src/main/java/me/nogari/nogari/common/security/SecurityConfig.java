801cbb15551f655b2f923d1c489bd5d4ef4a352a
임지원 <57473228+wonwu@users.noreply.github.com>
2023-04-18 13:40:18 +0900

    [BE] feat: 토큰 spring security 적용
    이슈: S08P31C206-107

--- back/src/main/java/me/nogari/nogari/common/security/SecurityConfig.java
+++ back/src/main/java/me/nogari/nogari/common/security/SecurityConfig.java
@@ -64,6 +64,7 @@ public class SecurityConfig {
 			.antMatchers("/members/signup", "/members/login", "/members/duplicate", "/refresh").permitAll()
 			.antMatchers("/admin/**").hasRole("ADMIN")
 			.antMatchers("/user/**").hasRole("USER")
+			.antMatchers("/oauth/**").hasRole("USER")
 			.anyRequest().denyAll()
 			.and()
 			// JWT 인증 필터 적용
