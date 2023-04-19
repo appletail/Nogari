e148ac1c54ee18020efea39e135e936965a69064
박시형 <qkrtlgud3286@naver.com>
2023-04-19 14:02:23 +0900

    [BE] fix: entity 1차 수정
    - tistory entity 수정
    - velog entity 삭제
    - Tag entity 삭제
    - 관련 repository 삭제
    
    이슈: S08P31C206-116

--- back/src/main/java/me/nogari/nogari/repository/VelogTagRepository.java
+++
@@ -1,8 +0,0 @@
-package me.nogari.nogari.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import me.nogari.nogari.entity.VelogTag;
-
-public interface VelogTagRepository extends JpaRepository<VelogTag, Long> {
-}
