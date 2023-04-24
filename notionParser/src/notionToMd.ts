232829ed289ff39a5411986a920a15aa471cc2ea
정하림 <harimchung1998@gmail.com>
2023-04-24 10:32:10 +0900

    [BE] notion parse 함수 변경
    이슈: S08P31C206-

--- notionParser/src/notionToMd.ts
+++ notionParser/src/notionToMd.ts
@@ -1,65 +1,43 @@
-import { Client } from "@notionhq/client"
-<<<<<<< HEAD
-const { NotionToMarkdown } = require("notion-to-md");
-import { writeFileSync } from 'fs';
+import { Client } from "@notionhq/client";
+// const { NotionToMarkdown } = require("notion-to-md");
+import { writeFileSync } from "fs";
 // or
 // import { NotionToMarkdown } from "notion-to-md";
 import photoToTistory from "./photoToTistory";
 
-
-
-const notion = new Client({
-  auth: "secret_W2tdauqeesSK7auwcOvo9dMYgmdffBW0IRXD94LszDg",
-});
-
-// passing notion client to the option
-const n2m = new NotionToMarkdown({ notionClient: notion });
-const block_id = "1318cd9451fd4a6ab5241441f17d99e6";
-
-
-(async () => {
-  // const mdblocks = await n2m.pageToMarkdown(block_id);
-  // console.log(mdblocks)
-  const {results} = await notion.blocks.children.list({
-    block_id,
-  })
-  // n2m.setCustomTransformer("paragraph", async (block:any) => {
-  //   const {paragraph} = block as any;
-  //   console.log(paragraph)
-  //   const color = paragraph.rich_text?.annotations
-  //   // console.log(color)
-  //   if (color === "yellow_background") {
-  //     return `<span style="background-color:#fff5n1">
-  //     ${await n2m.blocksToMarkdown(results)}</span>`
-  //   }
-  //   else if (color != "default") {
-  //     return `<span style="color:${color}"> hello world! </span>`
-  //   }
-  //   // return false;
-  // })
-  const x = await n2m.blocksToMarkdown(results);
-  // console.log(x);
-  const mdString = n2m.toMarkdownString(x);
-  
-  // writing to file
-  writeFileSync("harim2.md", mdString);
-=======
 import { NotionToMarkdown } from "notion-to-md";
-import { writeFileSync } from 'fs';
 import customNotionToMarkdown from "./utils/customNotionToMarkdown";
 
-const notion = new Client({
-  auth: "사용자 엑세스 토큰",
-});
+interface InotionToMd {
+  notionToken: string;
+  blockId: string;
+}
 
-// passing notion client to the option
-const custom_n2m = customNotionToMarkdown(notion);
+const notionToMd = ({ notionToken, blockId }: InotionToMd) => {
+  const notion = new Client({
+    auth: notionToken,
+  });
 
-(async () => {
-  const mdblocks = await custom_n2m.pageToMarkdown("노션 페이지 혹은 블럭 고유 id");
-  const mdString = custom_n2m.toMarkdownString(mdblocks);
+  // passing notion client to the option
+  const custom_n2m = customNotionToMarkdown(notion);
 
-  //writing to file
-  writeFileSync("test.md", mdString);
->>>>>>> dev
-})();
+  (async () => {
+    const mdblocks = await custom_n2m.pageToMarkdown(blockId);
+    const mdString = custom_n2m.toMarkdownString(mdblocks);
+
+    //writing to file
+    // console.log(mdString);
+
+    // file 형태로 내보내기
+    // writeFileSync("test.md", mdString);
+    return mdString;
+  })();
+};
+
+// sample 처럼 object 형태로 함수를 호출하시면 됩니다.
+
+const sample = {
+  notionToken: "사용자토큰",
+  blockId: "페이지 url 뒤의 block ID",
+};
+notionToMd(sample);
