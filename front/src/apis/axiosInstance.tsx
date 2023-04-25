49093fc61184c48192fdf1b624e60e459eec03bf
정하림 <harimchung1998@gmail.com>
2023-04-25 10:33:51 +0900

    [FE] fix: api 연결 시 env 파일 추가
    이슈: S08P31C206-

--- front/src/apis/axiosInstance.tsx
+++ front/src/apis/axiosInstance.tsx
@@ -1,35 +1,34 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance } from 'axios'
 
 export const interceptors = (instance: AxiosInstance) => {
   instance.interceptors.request.use(
     (config) => {
       // access token 헤더 삽입 코드
-      const token = localStorage.getItem("accessToken");
-      config.headers.Authorization = `Bearer ${token}`;
-      return config;
+      const token = localStorage.getItem('accessToken')
+      config.headers.Authorization = `Bearer ${token}`
+      return config
     },
     (error) => Promise.reject(error.response)
-  );
-  return instance;
-};
+  )
+  return instance
+}
 
-const BASE_URL = "http://localhost:8080/api/v1"; // 로컬 서버
-// const BASE_URL = ""; // 배포 서버
+const BASE_URL = `${import.meta.env.VITE_SERVER_URL}` // 로컬 서버
 
 // 단순 get요청으로 인증값이 필요없는 경우
 const axiosApi = (url: string, options?: object) => {
-  const instance = axios.create({ baseURL: url, ...options });
-  return instance;
-};
+  const instance = axios.create({ baseURL: url, ...options })
+  return instance
+}
 
 // post, delete등 api요청 시 인증값이 필요한 경우
 const axiosAuthApi = (url: string, options?: object) => {
-  const instance = axios.create({ baseURL: url, ...options });
-  interceptors(instance);
-  return instance;
-};
+  const instance = axios.create({ baseURL: url, ...options })
+  interceptors(instance)
+  return instance
+}
 
-export const axBase = axiosApi(BASE_URL);
-export const axAuth = axiosAuthApi(BASE_URL);
+export const axBase = axiosApi(BASE_URL)
+export const axAuth = axiosAuthApi(BASE_URL)
 
 // refresh token 으로 갱신 필요한 경우
