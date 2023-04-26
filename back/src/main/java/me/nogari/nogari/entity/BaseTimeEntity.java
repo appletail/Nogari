62bf18d4296712adf76cf189626469f1d1c9b3ec
kakaopanda <kakao_panda@naver.com>
2023-04-26 11:48:38 +0900

    [BE] feat: LocalDateTime ERD Type 및 Format 변경
    - LocalDateTime Format 변경(yyyy-MM-dd HH:mm:ss)
    
    이슈: S08P31C206-174

--- back/src/main/java/me/nogari/nogari/entity/BaseTimeEntity.java
+++ back/src/main/java/me/nogari/nogari/entity/BaseTimeEntity.java
@@ -1,10 +1,13 @@
 package me.nogari.nogari.entity;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -25,9 +28,24 @@ public abstract class BaseTimeEntity {
 
 	@CreatedDate
 	@Column(name = "created_at", updatable = false)
-	private LocalDateTime createdDate;
+	private String createdDate;
 
 	@LastModifiedDate
 	@Column(name = "modified_at")
-	private LocalDateTime modifiedDate;
+	private String modifiedDate;
+
+	@PrePersist
+	public void onPrePersist() {
+		String customLocalDateTimeFormat = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+		// LocalDateTime parsedCreateDate = LocalDateTime.parse(customLocalDateTimeFormat, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+		this.createdDate = customLocalDateTimeFormat;
+		this.modifiedDate = customLocalDateTimeFormat;
+	}
+
+	@PreUpdate
+	public void onPreUpdate() {
+		String customLocalDateTimeFormat = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+		// LocalDateTime parsedModifiedDate = LocalDateTime.parse(customLocalDateTimeFormat, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+		this.modifiedDate = customLocalDateTimeFormat;
+	}
 }
