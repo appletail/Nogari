e335a22bb8a118a868e570f036194699a0dea2db
정하림 <harimchung1998@gmail.com>
2023-05-08 09:27:00 +0900

    [FE] feat: axios instance 추가
    이슈: S08P31C206-

--- front/src/apis/axiosInstance.tsx
+++ front/src/apis/axiosInstance.tsx
@@ -1,29 +1,5 @@
 import axios, { AxiosInstance } from 'axios'
 
-// refresh token 으로 갱신 필요한 경우
-function getRefreshToken() {
-  const response = axAuth.post('/members/refresh', {
-    access_token: sessionStorage.getItem('accessToken'),
-    refresh_token: sessionStorage.getItem('refreshToken'),
-  })
-  return response
-}
-
-export const interceptors = (instance: AxiosInstance) => {
-  instance.interceptors.response.use(
-    (response) => {
-      console.log('interceptor success')
-      return response
-    },
-    async (error) => {
-      console.log('interceptor error')
-      console.log(error)
-    }
-  )
-
-  return instance
-}
-
 const BASE_URL = `${import.meta.env.VITE_SERVER_URL}` // 로컬 서버
 
 // 단순 get요청으로 인증값이 필요없는 경우
@@ -45,5 +21,44 @@ const axiosAuthApi = (url: string, options?: object) => {
   return instance
 }
 
+// refresh token 으로 갱신 필요한 경우
+function postRefreshToken() {
+  const response = axBase.post('/members/refresh', {
+    access_token: sessionStorage.getItem('accessToken'),
+    refresh_token: sessionStorage.getItem('refreshToken'),
+  })
+  return response
+}
+
+export const interceptors = (instance: AxiosInstance) => {
+  instance.interceptors.response.use(
+    async (response) => {
+      const { config } = response
+
+      if (response?.data?.resultCode !== 200) {
+        const originRequest = config
+        try {
+          const tokenResponse = await postRefreshToken()
+          console.log(tokenResponse.data)
+          // if (tokenResponse.data?.resultCode === 200) {
+          //   console.log(tokenResponse.data?.resultMessage)
+          // } else {
+          //   console.log('---------------------')
+          //   console.log(tokenResponse?.data?.resultMessage)
+          // }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      return response
+    },
+    async (error) => {
+      console.log('interceptor error')
+      console.log(error)
+    }
+  )
+
+  return instance
+}
 export const axBase = axiosApi(BASE_URL)
 export const axAuth = axiosAuthApi(BASE_URL)
--- front/src/pages/oauth/TistoryOAuth.tsx
+++ front/src/pages/oauth/TistoryOAuth.tsx
@@ -1,15 +1,8 @@
 import { useEffect } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 
+// import { axAuth } from '@/apis/axiosInstance'
 import { postOauthTistory } from '@/apis/OauthApis'
-const postTistory = async (code: string) => {
-  try {
-    const response = await postOauthTistory(code)
-    console.log(response)
-  } catch (error) {
-    console.log(error)
-  }
-}
 
 function TistoryOAuth() {
   const [searchParams] = useSearchParams()
@@ -17,18 +10,17 @@ function TistoryOAuth() {
 
   useEffect(() => {
     const code = searchParams.get('code')
-    if (code) {
-      postTistory(code)
-    }
-    // axAuth({
-    //   method: 'post',
-    //   url: '/oauth/tistory',
-    //   data: { code },
-    // })
-    //   .then(() => sessionStorage.setItem('tistory', 'true'))
-    //   .then(() => navigate('/tistory'))
-    //   .catch((err) => console.log(err))
-  }, [])
+    ;(async function () {
+      if (code) {
+        try {
+          const response = await postOauthTistory(code)
+          console.log(response)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    })()
+  }, [searchParams])
   return <div>TistoryOAuth</div>
 }
 
