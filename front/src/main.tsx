f9edac0326a2ee7a77545e1d37cd67c8d7211d39
정하림 <harimchung1998@gmail.com>
2023-05-09 14:18:26 +0900

    [FE] feat: react query 설치
    이슈: S08P31C206-

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,18 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '@/styles/global.css'
 import { HelmetProvider } from 'react-helmet-async'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { RouterProvider } from 'react-router-dom'
 
 import routers from './routes'
 // theme
 import ThemeProvider from './theme'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
-  <HelmetProvider>
-    <ThemeProvider>
-      <RouterProvider router={routers} />
-    </ThemeProvider>
-  </HelmetProvider>
+  <QueryClientProvider client={queryClient}>
+    <HelmetProvider>
+      <ThemeProvider>
+        <RouterProvider router={routers} />
+      </ThemeProvider>
+    </HelmetProvider>
+  </QueryClientProvider>
   // </React.StrictMode>
 )
