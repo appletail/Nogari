590bc99b55a9ca0f6a771a95aa9df54bf16a63ff
박시형 <qkrtlgud3286@naver.com>
2023-05-09 17:43:54 +0900

    [BE] fix: redisconfig 패스워드 추가
    - redis 패스워드 추가
    
    이슈: S08P31C206-274

--- back/src/main/java/me/nogari/nogari/api/service/MemberServiceImpl.java
+++ back/src/main/java/me/nogari/nogari/api/service/MemberServiceImpl.java
@@ -120,7 +120,7 @@ public class MemberServiceImpl implements MemberService {
 			JWT.builder()
 				.id(member.getMemberId())
 				.refresh_token(UUID.randomUUID().toString())
-				.expiration(180) // refresh 만료기간 2주
+				.expiration(14) // refresh 만료기간 2주
 				.build()
 		);
 		return jwt.getRefresh_token();
