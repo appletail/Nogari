f3057dd299b5bf49629ff3d086aa9366de9363cf
kakaopanda <kakao_panda@naver.com>
2023-05-17 13:01:56 +0900

    [FE] feat: 로그인 페이지 CSS 수정
    이슈: S08P31C206-

--- chromeExtension/src/pages/Login.tsx
+++ chromeExtension/src/pages/Login.tsx
@@ -35,28 +35,56 @@ function Login() {
 
   return (
     <div className={style.Container}>
+      <img src="src/assets/logos/NogariLogo.png" style={{
+        width:'70%', 
+        marginTop:'30px',
+        marginBottom:'50px',
+        marginLeft: 'auto',
+        marginRight: 'auto'
+        }}>
+      </img>
       <form className={style.LoginForm} onSubmit={handleSubmit(submitHandler)}>
-        <input type="email" {...register('email', { required: true })} placeholder="Email" />
+        <input type="email" {...register('email', { required: true })} placeholder="이메일(E-mail)" style={{
+              width:'70%',
+              marginBottom: '20px', 
+              marginLeft: 'auto',
+              marginRight: 'auto'
+              }}/>
         {errors.email && <span>이메일을 입력해주세요.</span>}
         <div>
           <input
             id="password"
             type={showPasseword}
             {...register('password', { required: true })}
-            placeholder="Password"
+            placeholder="비밀번호(Password)" style={{
+              width:'70%', 
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              }}
           />
           <label
             htmlFor="password"
             onClick={() => {
               showPasseword === 'password' ? setShowPassword('text') : setShowPassword('password')
             }}
-          >
+            >
             {showPasseword}
           </label>
         </div>
         {errors.password && <span>비밀번호를 입력해주세요.</span>}
 
-        <button type="submit">Login</button>
+        <button type="submit" style={{
+        width:'80%', 
+        marginTop:'40px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        border: '0',
+        backgroundColor: 'transparent'
+        }}><img src="src/assets/logos/LoginButton.svg" style={{
+          width:'100%', 
+          marginLeft: 'auto',
+          marginRight: 'auto'
+          }}/></button>
       </form>
     </div>
   )
